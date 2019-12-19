29520d48433dc07173b3c52bf72c22970f8657da
Marshall Thompson <marshall@creativeideal.net>
2019-12-19 02:21:24 -0700

    Add feathers-chat stylesheets

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './assets/styles/base.css'
+import './assets/styles/chat.css'
+
 Vue.config.productionTip = false
 
 new Vue({
