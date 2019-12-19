1ebaf15de5eb34b1dc6d490ac9a3330719eb5820
Marshall Thompson <marshall@creativeideal.net>
2019-12-19 02:15:48 -0700

    Setup the vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { FeathersVuex } from '../feathers-client'
+import auth from './store.auth'
 
 Vue.use(Vuex)
+Vue.use(FeathersVuex)
+
+const requireModule = require.context(
+  // The path where the service modules live
+  './services',
+  // Whether to look in subfolders
+  false,
+  // Only include .js files (prevents duplicate imports`)
+  /.js$/
+)
+const servicePlugins = requireModule
+  .keys()
+  .map(modulePath => requireModule(modulePath).default)
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  plugins: [...servicePlugins, auth]
 })
