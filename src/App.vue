ae4c26a2031ed73f44c662c023ec734a3a41ba51
Marshall Thompson <marshall@creativeideal.net>
2019-12-19 02:34:03 -0700

    Update App.vue
    Removes default styling of App.vue and adds the vue composition API for auth logic.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
+<script>
+import { onMounted, watch } from '@vue/composition-api'
+
+export default {
+  name: 'App',
+  setup(props, context) {
+    const { $store } = context.root
+    const { user } = $store.state.auth
+
+    // Redirect to chat page if there's a user, otherwise to login page.
+    watch(
+      () => user,
+      user => {
+        const toRouteName = user ? 'Chat' : 'Login'
+        this.$router.replace({ name: toRouteName })
+      }
+    )
+
+    // Attempt jwt auth when the app mounts.
+    onMounted(() => {
+      $store.dispatch('auth/authenticate').catch(error => {
+        if (!error.message.includes('Could not find stored JWT')) {
+          console.error(error)
+        }
+      })
+    })
+
+    return {}
+  }
+}
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
