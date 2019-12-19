420fab1565e99d5cf9b21c8a94289d4dfc8fcab3
Marshall Thompson <marshall@creativeideal.net>
2019-12-19 03:05:16 -0700

    fix: forgot to Vue.use the composition api

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
+Vue.use(VueCompositionApi)
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
