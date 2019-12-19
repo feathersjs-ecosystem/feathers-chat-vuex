d91cd5324ac36f2754c5468bd673525426c234ee
Marshall Thompson <marshall@creativeideal.net>
2019-12-19 02:08:15 -0700

    fix eslint config and yarn lint —fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,28 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        fix: true,
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none'
+      }
+    ]
   },
+
   parserOptions: {
-    parser: "babel-eslint"
-  }
-};
+    parser: 'babel-eslint'
+  },
+
+  extends: ['plugin:vue/recommended', '@vue/prettier']
+}
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -104,11 +104,12 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
+    // eslint-disable-next-line vue/require-default-prop
     msg: String
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
